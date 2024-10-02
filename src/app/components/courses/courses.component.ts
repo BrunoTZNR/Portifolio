531565815a1342a18ce06e06e89e4c2be94d7bcb
BrunoTZNR <brunomarcodasilva@gmail.com>
2024-10-02 16:53:12 -0300

    Update: Update data

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -9,8 +9,8 @@ import { Course } from 'src/app/types/course.interface';
 export class CoursesComponent {
 
   courses: Course[] = [
+    { type: 'graduation', institution: 'Faculdade Estácio', dtEnd: '2026', desc: 'Ciência da Computação' },
     { type: 'graduation', institution: 'Faculdade Projeção', dtEnd: '2024', desc: 'Análise e Desenvolvimento de Sistemas' },
-    { type: 'graduation', institution: 'Faculdade Projeção', dtEnd: '2026', desc: 'Sistemas de Informação' },
     { type: 'course', institution: 'Fundação Bradesco', dtEnd: '2023', desc: 'Lei Geral de Proteção de Dados' },
     { type: 'course', institution: 'Fundação Bradesco', dtEnd: '2023', desc: 'Segurança em Tecnologia da informação' },
     { type: 'course', institution: 'Senai - DF', dtEnd: '2021', desc: 'Lógica de programação' },
